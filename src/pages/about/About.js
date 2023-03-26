44304a9f2a8388326881267f18ea2a851f52d268
aya <aya.gamal1794@gmail.com>
2023-03-27 01:22:15 +0200

    first

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,7 +2,6 @@ import Nav from "../../components/nav/Nav";
 import Footer from "../../components/footer/Footer";
 import "./about.css";
 import {useTranslation} from "react-i18next";
-
 function About(){
     const {t, i18n} = useTranslation();
     return (
@@ -15,7 +14,7 @@ function About(){
                 <div className="about-body">
                     <div className="container">
                         <div className="row">
-                            <img className = "col-md-6 col-xs-12" src="./images/about.jpg" alt="makeup"/>
+                            <img className = "col-md-6 col-xs-12" src={"./images/about.jpg"} alt="makeup"/>
                             <p className = "col-md-6 col-xs-12" >{t("longp")}</p>
                         </div>
                     </div>
